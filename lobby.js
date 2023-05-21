8e03d2cb8decfb46930d71b8611f76c5eb4a533b
Valentin Vlad <55762636+vvalentinv@users.noreply.github.com>
2023-05-21 08:21:42 -0400

    added behavior on accepting challenge

--- lobby.js
+++ lobby.js
@@ -4,7 +4,8 @@ let welcomeUser = document.getElementById('welcome-user');
 let loginStatusButton = document.getElementById('login-status');
 let header3 = document.getElementById('header3');
 let header2 = document.getElementById('header2');
-let newReimb = document.getElementById('new-reimb');
+let unchallengedBattles = document.getElementById('unchallenged-battles');
+let setDefense = document.getElementById('set-defense');
 let tbody = document.getElementById('battle-tbl-tbody');
 let submitButton = document.getElementById('submit-btn');
 let cancelButton = document.getElementById('cancel-btn');
@@ -32,11 +33,12 @@ const grabDataAndFeedtoPage = async () => {
     if (res.status == 200) {
       data = await res.json();
       console.log(data);
-      if (data.role == 1) {
-        header3.removeAttribute('hidden');
+      if (data.battles.message == 'Finish your current battle engagement, before attempting a new one!') {
+        unchallengedBattles.innerHTML = '';
+        setDefense.removeAttribute('hidden');
       }
       welcome.innerHTML = `Welcome back <a id="welcome-user" class="navbar-brand" href="#">` + data.user + `</a>`;
-      addBattlesToTable(data);
+      addBattlesToTable(data.battles);
     }
     if (res.status == 401) {
       window.location.href = '/index.html';
@@ -162,6 +164,7 @@ filter.addEventListener('change', async (e) => {
         'Access-Control-Allow-Credentials': 'true'
       }
     })
+
     data = await res.json();
     addReimbursementsToTable(data);
   } catch (err) {
@@ -173,14 +176,47 @@ filter.addEventListener('change', async (e) => {
   }
 });
 
+document.addEventListener('click', (e) => {
+  e.stopPropagation();
+  e.target.addEventListener('change', async (e) => {
+    if (String(e.target.innerHTML).includes('class="accepted dropdown-item"')) {
+      console.log(e.target.value)
+      while (tbody.hasChildNodes()) {
+        tbody.removeChild(tbody.lastChild);
+      }
+      try {
+        let res = await fetch(url + `/battles/` + e.target.value + `?accepted=True`, {
+          'credentials': 'include',
+          'method': 'PUT',
+          'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Credentials': 'true'
+          }
+        })
+        if (res.status == 200) {
+          let data = await res.json();
+          success.removeAttribute('hidden');
+          success.innerText = data.message;
+          setTimeout(() => { window.location.reload(); }, 3000)
+        }
+      } catch (err) {
+        if (err.message == "Failed to fetch") {
+          welcome.innerHTML = "Server unreachable: contact IT Admin";
+          welcome.style.color = 'red';
+          welcome.style.fontWeight = 'bold';
+        }
+      }
+    }
+  })
+})
 
 function addBattlesToTable(data) {
   for (b of data.battles) {
     let row = document.createElement('tr');
 
     let playerName = document.createElement('td');
-    playerName.innerHTML = `<select name="accept-challenge-in-row" class="filter-in-row"> <option class="my-class dropdown-item" value=1` +
-      `>` + b[1] + `</option> <option class="my-class dropdown-item" value="` + b[0] + `">Accept Challenge</option>`;    // playerName.innerHTML = b[1];
+    playerName.innerHTML = `<select name="accept-challenge-in-row" class="filter-in-row"> <option class="accepted dropdown-item" value=1` +
+      `>` + b[1] + `</option> <option class="accepted dropdown-item" value="` + b[0] + `">Accept Challenge</option>`;    // playerName.innerHTML = b[1];
     let defenseSize = document.createElement('td');
     defenseSize.innerHTML = b[2];
     // let status_nameCell = document.createElement('td');
