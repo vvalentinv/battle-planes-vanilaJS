aa516da66b2ddcca7e97bf29136628543c3e98a9
Valentin Vlad <55762636+vvalentinv@users.noreply.github.com>
2023-06-01 05:38:55 -0400

    refined challenge params set/reset, added event for plane evaluation

--- challenge.js
+++ challenge.js
@@ -7,11 +7,12 @@ let setDefense = document.getElementById('set-defense');
 let defenseSky = document.getElementById('defense-sky');
 let setParams = document.getElementById('set-params-btn');
 let resetParams = document.getElementById('reset-params-btn');
-let submitButton = document.getElementById('submit-btn');
-let cancelButton = document.getElementById('cancel-btn');
+let evaluatePlane = document.getElementById('evaluate-btn');
+let openChallenge = document.getElementById('open-btn');
 let error = document.getElementById('error-message');
 let serverError = document.getElementById('sever-error');
-let success = document.getElementById('success-messages');
+let successMessage = document.getElementById('success-message');
+let paramMessage = document.getElementById('params-message');
 let remainingPlanes = document.getElementById('remaining-planes');
 let flightDirection = document.getElementById('flight-direction');
 let cockpitValue = document.getElementById('cockpit-value');
@@ -33,13 +34,18 @@ let expirationTime = null;
 
 
 setParams.addEventListener('click', () => {
-  if (skySize.value != 0 || defenseSize.value != 0 || maxTime.value > 0) {
+  if (skySize.value != 0 && defenseSize.value != 0 && maxTime.value > 0) {
     sky = parseInt(skySize.value);
     defenseSz = parseInt(defenseSize.value);
     expirationTime = parseInt(maxTime.value);
-    //build defense grid
     defense();
     skyCells = document.getElementsByClassName('grid-cell');
+    skySize.setAttribute('disabled', true);
+    defenseSize.setAttribute('disabled', true);
+  } else {
+    paramMessage.innerText = 'Please set all parameters!';
+    paramMessage.removeAttribute('hidden');
+    paramMessage.style.color = 'red';
   }
 });
 
@@ -53,6 +59,9 @@ resetParams.addEventListener('click', () => {
   while (defenseSky.hasChildNodes()) {
     defenseSky.removeChild(defenseSky.lastChild);
   }
+  skySize.removeAttribute('disabled');
+  defenseSize.removeAttribute('disabled');
+  existingDefense = [];
 });
 
 // const grabDataAndFeedtoPage = async () => {
@@ -183,180 +192,60 @@ resetParams.addEventListener('click', () => {
 //   spinner2.setAttribute('hidden', true);
 // })
 
-// submitButton.addEventListener('click', async () => {
+evaluatePlane.addEventListener('click', async () => {
+  console.log("coockpitCoords cockpitVal Direction", cockpitCoordinates.value, cockpitValue.value, flightDirection.value);
+  if (!cockpitCoordinates.value || !cockpitValue.value || flightDirection.value === "0") {
+    planeMessage.innerText = "flight direction and cockpit coordinates are required fields!";
+    planeMessage.style.color = 'red';
+  } else {
+    while (defenseSky.hasChildNodes()) {
+      defenseSky.removeChild(defenseSky.lastChild);
+    }
+    try {
+      spinner2.removeAttribute('hidden');
+      let res = await fetch(url + `/planes`, {
+        'credentials': 'include',
+        'method': 'GET',
+        'headers': {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Credentials': 'true'
+        },
+        'body': JSON.stringify({
+          "cockpit": cockpitValue.value,
+          "direction": flightDirection.value,
+          "sky": data.battles.battles[0][3]
+        })
+      })
+      if (res.status == 200) {
+        let data = await res.json();
+        spinner2.setAttribute('hidden', true);
+        if (data.message != "No Match") {
+          existingDefense.push(parseInt(data.message));
+        } else {
+        }
 
-//   spinner2.removeAttribute('hidden');
-//   console.log("coockpitCoords cockpitVal Direction", cockpitCoordinates.value, cockpitValue.value, flightDirection.value);
-//   if (!cockpitCoordinates.value || !cockpitValue.value || flightDirection.value == 0) {
+      } else if (res.status == 400) {
+        let data = await res.json();
+        spinner2.setAttribute('hidden', true);
+        planeMessage.innerText = data.message;
+        planeMessage.style.color = 'red';
 
-//     planeMessage.innerText = "flight direction and cockpit coordinates are required fields!";
-//     planeMessage.style.color = 'red';
-//   } else {
-//     while (defenseSky.hasChildNodes()) {
-//       defenseSky.removeChild(defenseSky.lastChild);
-//     }
-//     try {
+      } else if (res.status == 401) {
+        spinner2.setAttribute('hidden', true);
+        window.location.href = '/index.html';
+      }
+    } catch (err) {
+      if (err.message == "Failed to fetch") {
+        success.innerHTML = "Server unreachable: contact site admin";
+        success.style.color = 'red';
+        success.style.fontWeight = 'bold';
+      }
+    }
+  }
+})
 
-//       spinner2.removeAttribute('hidden');
-//       let res = await fetch(url + `/battles/` + data.battles.battles[0][0] + `?defense=True`, {
-//         'credentials': 'include',
-//         'method': 'PUT',
-//         'headers': {
-//           'Content-Type': 'application/json',
-//           'Access-Control-Allow-Credentials': 'true'
-//         },
-//         'body': JSON.stringify({
-//           "cockpit": cockpitValue.value,
-//           "direction": flightDirection.value,
-//           "sky": data.battles.battles[0][3]
-//         })
-//       })
-//       if (res.status == 200) {
-//         let data = await res.json();
-//         spinner2.setAttribute('hidden', true);
-//         if (data.message != "Defense setup complete!") {
-//           // success.removeAttribute('hidden');
-//           // success.innerText = data.message;
-//           setTimeout(() => {
-//             cancelButton.click();
-//             // success.setAttribute('hidden', true);
-//           }, 1000)
-//         } else {
-//           window.location.href = '/game.html';
-//         }
 
-//       } else if (res.status == 400 || res.status == 403) {
-//         let data = await res.json();
-//         spinner2.setAttribute('hidden', true);
-//         if (data.message == "Time frame to add planes for defense setup elapsed.") {
-//           planeMessage.innerText = data.message;
-//           planeMessage.style.color = 'red';
-//           setTimeout(() => {
-//             window.location.reload();
-//           }, 2000);
-//         }
-//         planeMessage.innerText = data.message;
-//         planeMessage.style.color = 'red';
-//         setTimeout(() => {
-//           cancelButton.click();
-//         }, 2000);
 
-//       } if (res.status == 401) {
-//         spinner2.setAttribute('hidden', true);
-//         window.location.href = '/index.html';
-//       }
-//     } catch (err) {
-//       if (err.message == "Failed to fetch") {
-//         success.innerHTML = "Server unreachable: contact IT Admin";
-//         success.style.color = 'red';
-//         success.style.fontWeight = 'bold';
-//       }
-//     }
-//   }
-// })
-
-// filter.addEventListener('change', async (e) => {
-
-//   while (tbody.hasChildNodes()) {
-//     tbody.removeChild(tbody.lastChild);
-//   }
-//   try {
-//     let res = await fetch(url + "reimbursements?status=" + filter.value, {
-//       'credentials': 'include',
-//       'method': 'GET',
-//       'headers': {
-//         'Content-Type': 'application/json',
-//         'Access-Control-Allow-Credentials': 'true'
-//       }
-//     })
-
-//     data = await res.json();
-//     addReimbursementsToTable(data);
-//   } catch (err) {
-//     if (err.message == "Failed to fetch") {
-//       success.innerHTML = "Server unreachable: contact IT Admin";
-//       success.style.color = 'red';
-//       success.style.fontWeight = 'bold';
-//     }
-//   }
-// });
-
-// document.addEventListener('click', (e) => {
-//   e.stopPropagation();
-//   e.target.addEventListener('change', async (e) => {
-//     if (String(e.target.innerHTML).includes('class="accepted dropdown-item"')) {
-//       console.log(e.target.value)
-//       while (tbody.hasChildNodes()) {
-//         tbody.removeChild(tbody.lastChild);
-//       }
-//       try {
-//         spinner1.removeAttribute('hidden');
-//         let res = await fetch(url + `/battles/` + e.target.value + `?accepted=True`, {
-//           'credentials': 'include',
-//           'method': 'PUT',
-//           'headers': {
-//             'Content-Type': 'application/json',
-//             'Access-Control-Allow-Credentials': 'true'
-//           }
-//         })
-//         if (res.status == 200) {
-//           let data = await res.json();
-//           spinner1.setAttribute('hidden', true);
-//           success.removeAttribute('hidden');
-//           success.innerText = data.message;
-//           setTimeout(() => { window.location.reload(); }, 1000)
-//         }
-//       } catch (err) {
-//         if (err.message == "Failed to fetch") {
-//           success.removeAttribute('hidden');
-//           success.innerText = "Server unreachable: contact IT Admin";
-//           success.style.color = 'red';
-//           success.style.fontWeight = 'bold';
-//         }
-//         else {
-//           console.log(err)
-//         }
-//       }
-//     }
-//   })
-// })
-
-// function addBattlesToTable(data) {
-//   for (b of data.battles.battles) {
-//     let row = document.createElement('tr');
-
-//     let playerName = document.createElement('td');
-//     playerName.innerHTML = `<select name="accept-challenge-in-row" class="filter-in-row" style="background: gainsboro;"> <option class="accepted dropdown-item" value=1` +
-//       `>` + b[2] + `</option> <option class="accepted dropdown-item" value="` + b[0] + `">Accept Challenge</option>`;    // playerName.innerHTML = b[1];
-//     let defenseSize = document.createElement('td');
-//     defenseSize.innerHTML = b[1];
-//     let skySize = document.createElement('td');
-//     skySize.innerHTML = b[3];
-//     let time = document.createElement('td');
-//     const timer = setInterval(() => {
-//       let now = new Date().getTime();
-//       let startDate = Date.parse(b[4]);
-//       let distance = startDate - now + 4 * 3600000;
-//       let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//       hours < 10 ? hours = "0" + hours : hours;
-//       let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-//       minutes < 10 ? minutes = "0" + minutes : minutes;
-//       let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-//       seconds < 10 ? seconds = "0" + seconds : seconds;
-//       time.innerText = hours + ":" + minutes + ":" + seconds;
-//       if (distance < 0) {
-//         clearInterval(timer);
-//         time.innerText = "EXPIRED";
-//       }
-//     }, 1000);
-
-//     row.appendChild(playerName);
-//     row.appendChild(defenseSize);
-//     row.appendChild(skySize);
-//     row.appendChild(time);
-//     tbody.appendChild(row);
-//   }
-// }
 
 const defense = () => {
   let layoutSize = sky + 1
@@ -389,15 +278,11 @@ const defense = () => {
     }
   }
   defenseSky.setAttribute('style', `grid-template-columns: repeat(` + layoutSize + `, auto);`)
-  //set remaining planes
-  // if (b[0][1]) {
-  //   let currentDefenseSize = b[0][1].length;
-  //   remainingPlanes.innerText = b[0][2] - currentDefenseSize;
-  // } else {
-  //   remainingPlanes.innerText = b[0][2];
-  // }
-
-
+  // set remaining planes
+  if (defenseSz) {
+    let currentDefenseSize = existingDefense.length;
+    remainingPlanes.innerText = defenseSz - currentDefenseSize;
+  }
 }
 
 const testPlacement = (e) => {
