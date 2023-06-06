34d9caa36efa0280e1966bb0f8bc63b45141859d
Valentin Vlad <55762636+vvalentinv@users.noreply.github.com>
2023-06-06 03:25:15 -0400

    added cards content on home page

--- lobby.js
+++ lobby.js
@@ -134,6 +134,7 @@ const displayDefense = (defense) => {
 }
 
 loginStatusButton.addEventListener('click', async () => {
+
   let res = await fetch(url + '/logout', {
     'credentials': 'include',
     'method': 'POST',
@@ -144,16 +145,10 @@ loginStatusButton.addEventListener('click', async () => {
   if (res.status == 200) {
     success.removeAttribute('hidden');
     success.innerText += "Thank you for playing!";
-    success.innerHTML += '<br><br>'
-    success.innerText += "Logging you out ";
-    success.innerHTML += '<br><br>'
+    setTimeout(() => { window.location.href = '/index.html'; }, 2000)
     for (let i = 0; i < 1500; i += 300) {
       setTimeout(() => { success.innerText += "."; }, i)
     }
-
-    setTimeout(() => { window.location.href = '/index.html'; }, 2000)
-
-
   }
 })
 
