837be34f7a197f1228005f7abb4d89107aac29a0
Valentin Vlad <55762636+vvalentinv@users.noreply.github.com>
2023-06-07 06:35:53 -0400

    added getStatus and concedeDefeat flows

--- battle.js
+++ battle.js
@@ -5,6 +5,7 @@ let welcomeUser = document.getElementById("welcome-user");
 let loginStatusButton = document.getElementById("login-status");
 let spinner = document.getElementById("spinner");
 let successMessage = document.getElementById("success-messages");
+let defeat = document.getElementById("concede-defeat");
 let url = `http://127.0.0.1:5000`;
 
 home.addEventListener("click", function() {
@@ -57,3 +58,67 @@ loginStatusButton.addEventListener('click', async () => {
     window.location.href = '/index.html';
   }
 })
+
+setInterval(getStatus, 3000);
+
+async function getStatus() {
+  try {
+    let res = await fetch(url + '/battles?defeat=False', {
+      'credentials': 'include',
+      'method': 'GET',
+      'headers': {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Credentials': 'true'
+      }
+    })
+    if (res.status == 200) {
+      data = await res.json();
+      console.log(data);
+      welcomeUser.innerText = data.user;
+    }
+    if (res.status == 401) {
+      window.location.href = '/index.html';
+    }
+  } catch (err) {
+    if (err.message == "Failed to fetch") {
+      successMessage.removeAttribute('hidden');
+      successMessage.innerText = "Server unreachable: contact site admin";
+      successMessage.style.color = 'red';
+      successM.style.fontWeight = 'bold';
+    }
+    else {
+      console.log(err)
+    }
+  }
+}
+
+async function concedeDefeat() {
+  try {
+    let res = await fetch(url + '/battles?defeat=True', {
+      'credentials': 'include',
+      'method': 'GET',
+      'headers': {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Credentials': 'true'
+      }
+    })
+    if (res.status == 200) {
+      data = await res.json();
+      console.log(data);
+      welcomeUser.innerText = data.user;
+    }
+    if (res.status == 401) {
+      window.location.href = '/index.html';
+    }
+  } catch (err) {
+    if (err.message == "Failed to fetch") {
+      successMessage.removeAttribute('hidden');
+      successMessage.innerText = "Server unreachable: contact site admin";
+      successMessage.style.color = 'red';
+      successM.style.fontWeight = 'bold';
+    }
+    else {
+      console.log(err)
+    }
+  }
+}
